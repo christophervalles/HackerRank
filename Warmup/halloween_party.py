2337f79f165ed773e77b78c2e934c153c69db858
Christopher Valles <info@christophervalles.com>
2014-09-07 21:36:24 +0100

    The love letter mister & halloween party

---
+++ Warmup/halloween_party.py
@@ -0,0 +1,5 @@
+t = int(raw_input())
+
+for l in range(0, t):
+    s = float(raw_input())
+    print int(s/2 * (s - s/2))
--- Warmup/the_love_letter_mistery.py
+++ Warmup/the_love_letter_mistery.py
@@ -1,5 +1,11 @@
+import math
+
 t = int(raw_input())
 
 for l in range(0, t):
-    s = float(raw_input())
-    print int(s/2 * (s - s/2))
+    s = raw_input()
+
+    v1 = s[0:int(math.floor(float(len(s))/2))]
+    v2 = s[int(math.ceil(float(len(s))/2)):][::-1]
+
+    print sum([abs(ord(v1[x]) - ord(v2[x])) for x in range(0, len(v1))])
